f78f8f0d98c599614d3fb60355108825e37ef42d
Rahul <rahulatrkm@gmail.com>
2019-05-13 22:22:33 +0530

    Completed Project

--- student_code.py
+++ student_code.py
@@ -1,34 +1,44 @@
-global intersection_dict, roads_list
-def shortest_path(M,start,goal):
-    global intersection_dict, roads_list
+import math
+
+global intersection_dict, roads_list, pos_path
+intersection_dict = {}
+roads_list = []
+pos_path = []
+def shortest_path(M, start, goal):
+    if start == goal:
+        return [start]
+    #print(M, start,goal)
+    global intersection_dict, roads_list, pos_path
     print("shortest path called")
-    #print(start, goal)
     intersection_dict = M.intersections
     roads_list = M.roads
-    current = start
-    path = []
-    frontiers = {}
-    # dictionary for O(1) look up
-    explored = {}
-    while current != goal:
-        # updating frontiers
-        for pos_road in road_list[current]:
-            # check if explored
-            temp = explored.get(pos_road, 0)
-            if not temp:
-                # dict with g & h value
-                frontiers{pos_road} = [(abs(intersection_dict[pos_road][0] - intersection_dict[current][0]) + abs(intersection_dict[pos_road][1] - intersection_dict[current][1])), (abs(intersection_dict[pos_road][0] - intersection_dict[goal][0]) + abs(intersection_dict[pos_road][1] - intersection_dict[goal][1]))]
-        
-        min_f = float('inf')
-        min_f_idx = 0
-        for curr_front in frontiers:
-            if sum(frontiers[curr_front]) < min_f:
-                min_f_idx = curr_front
-    return
+    pos_path = [[[],0,math.sqrt((intersection_dict[start][0] - intersection_dict[goal][0])**2 + abs(intersection_dict[start][1] - intersection_dict[goal][1])**2)]]
+    helper_path(start, goal)
+    if pos_path:
+        return pos_path[0][0]
+    else:
+        return "No possible path"
 
-def calc_min(current, frontiers):
-    global intersection_dict, roads_list
-    min_f = 0
-    g = []
-    h = []
-    for 
+def helper_path(current, goal):
+    global intersection_dict, roads_list, pos_path
+    #print(pos_path)
+    if not pos_path[0][0]:
+        pos_path[0][0].append(current)
+    elif pos_path[0][0][-1] == goal:
+        return
+    
+    #print(roads_list[current], 'road')
+    for front in roads_list[current]:
+        if front in pos_path[0][0]:
+            continue
+        #print(pos_path, "in for", front)
+        g = math.sqrt((intersection_dict[front][0] - intersection_dict[current][0])**2 + (intersection_dict[front][1] - intersection_dict[current][1])**2) + pos_path[0][-2]
+        h = math.sqrt((intersection_dict[front][0] - intersection_dict[goal][0])**2 + abs(intersection_dict[front][1] - intersection_dict[goal][1])**2)
+        pos_path.append([pos_path[0][0]+[front],g,h])
+    
+    del pos_path[0]
+    # handling if there is no any path
+    if not pos_path:
+        return
+    pos_path = sorted(pos_path, key = lambda x: x[1]+x[2])
+    helper_path(pos_path[0][0][-1], goal)
