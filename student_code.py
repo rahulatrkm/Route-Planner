91b7a830e442771b09d3d74a70f5d5d447aa5ce1
Rahul <rahulatrkm@gmail.com>
2019-05-14 10:55:08 +0530

    Implemented priority queue to reduce runtime.

--- student_code.py
+++ student_code.py
@@ -1,44 +1,74 @@
 import math
 
-global intersection_dict, roads_list, pos_path
+global intersection_dict, roads_list, pos_path, heuristic_values
+heuristic_values = {}
 intersection_dict = {}
 roads_list = []
 pos_path = []
+
+class  Priority_queue(object):
+    """docstring for  Priority_queue."""
+    def __init__(self):
+        self.queue = []
+    def __str__(self):
+        return ' '.join([str(i) for i in self.queue])
+    def isEmpty(self):
+        return not self.queue
+    def insert(self, data):
+        self.queue.append(data)
+    def delete(self):
+        try:
+            mini = 0
+            for i in range(len(self.queue)):
+                if (self.queue[i][-1]+self.queue[i][-2]) < (self.queue[mini][-1] + self.queue[mini][-2]):
+                    mini = i
+            item = self.queue[mini]
+            del self.queue[mini]
+            return item
+        except IndexError:
+            print()
+
 def shortest_path(M, start, goal):
     if start == goal:
         return [start]
+    
     #print(M, start,goal)
-    global intersection_dict, roads_list, pos_path
+    global intersection_dict, roads_list, pos_path, heuristic_values
+    heuristic_values = {}
+    for node in intersection_dict:
+        #print(node)
+        heuristic_values[node] = math.sqrt((intersection_dict[node][0] - intersection_dict[goal][0])**2 + abs(intersection_dict[node][1] - intersection_dict[goal][1])**2)
+        
+    #print(heuristic_values)
     print("shortest path called")
     intersection_dict = M.intersections
     roads_list = M.roads
-    pos_path = [[[],0,math.sqrt((intersection_dict[start][0] - intersection_dict[goal][0])**2 + abs(intersection_dict[start][1] - intersection_dict[goal][1])**2)]]
-    helper_path(start, goal)
-    if pos_path:
-        return pos_path[0][0]
-    else:
-        return "No possible path"
+    pos_path = Priority_queue()
+    pos_path.insert([[start],0,heuristic_values[start]])
+    return helper_path(start, goal)
 
 def helper_path(current, goal):
-    global intersection_dict, roads_list, pos_path
+    global intersection_dict, roads_list, pos_path, heuristic_values
     #print(pos_path)
-    if not pos_path[0][0]:
-        pos_path[0][0].append(current)
-    elif pos_path[0][0][-1] == goal:
-        return
-    
+    #print()
+    # handling if there is no any path
+    item = 0
+    if pos_path.isEmpty():
+        return "No possible path"
+    else:
+        item = pos_path.delete()
+    #print(item, "item")
+    current = item[0][-1]
+    if current == goal:
+        return item[0]
+
     #print(roads_list[current], 'road')
     for front in roads_list[current]:
-        if front in pos_path[0][0]:
+        if front in item[0]:
             continue
         #print(pos_path, "in for", front)
-        g = math.sqrt((intersection_dict[front][0] - intersection_dict[current][0])**2 + (intersection_dict[front][1] - intersection_dict[current][1])**2) + pos_path[0][-2]
-        h = math.sqrt((intersection_dict[front][0] - intersection_dict[goal][0])**2 + abs(intersection_dict[front][1] - intersection_dict[goal][1])**2)
-        pos_path.append([pos_path[0][0]+[front],g,h])
-    
-    del pos_path[0]
-    # handling if there is no any path
-    if not pos_path:
-        return
-    pos_path = sorted(pos_path, key = lambda x: x[1]+x[2])
-    helper_path(pos_path[0][0][-1], goal)
+        g = math.sqrt((intersection_dict[front][0] - intersection_dict[current][0])**2 + (intersection_dict[front][1] - intersection_dict[current][1])**2) + item[-2]
+        h = heuristic_values[front]
+        pos_path.insert([item[0]+[front],g,h])
+
+    return helper_path(current, goal)
